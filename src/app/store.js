293ff29e17683baf853f000952c2d9f76682d71e
vfa-tamtd <vfa.internship.tamtd@gmail.com>
2022-07-26 11:03:04 +0700

    init

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import postsReducer from '../features/posts/postsSlices'
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    posts: postsReducer,
+  },
 })
